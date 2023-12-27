ad4cb828e21f07b2262d9d7dad319334accfa597
websfonts <34331694+websfont@users.noreply.github.com>
2023-12-27 18:06:29 +0900

    dd

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Section1 from './components/Section1'
+import Section2 from './components/Section2'
+
+const App = () => {
+  return (
+    <div>
+      <Section1 />
+      <Section2 />
+    </div>
+  )
+}
+
+export default App
---
+++ portfolio/src/components/Section1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div id='section1' className='sec1-wrap'>
+        <iframe src="https://webstoryboy.github.io/web2022/webstoryboy_three/three.js/index64.html" frameborder="0" title='dd'></iframe>
+    </div>
+  )
+}
+
+export default Section1
